22eeacf2c73409c56a99ae699c4b52f3a844c63d
klimenko3007 <ek1830@tobii.com>
2022-01-30 13:54:12 +0100

    Updated the drawPixel function to take coordinates of x and y

--- index.js
+++ index.js
@@ -2,17 +2,32 @@
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d')
 
+// TODO: Relate to canvas size
+const Y_INDEX_BREAK = 400;
+
   
-const drawPixel = (_imageData, pixelNumber) => {
-  const firstNumber = (pixelNumber-1)*4
-  _imageData.data[firstNumber] = 255;  // R value
-  _imageData.data[firstNumber +1] = 0;    // G value
-  _imageData.data[firstNumber +2] = 0;  // B value
-  _imageData.data[firstNumber +3] = 255;
+const drawPixel = (_imageData, x, y) => {
+  const step = 4; //every pixel is R G B A, so we need 4 steps between each pixel
+  const one_row = Y_INDEX_BREAK*step; 
+  const x_index = x; 
+  const y_index = y*one_row; 
+  const firstIndex = x_index*step + y_index;
+
+  _imageData.data[firstIndex +0] = 255;  // R value
+  _imageData.data[firstIndex +1] = 0;    // G value
+  _imageData.data[firstIndex +2] = 0;  // B value
+  _imageData.data[firstIndex +3] = 255;
 }
   
 // TODO: replace magic numbers to get them from context
 const imageData = ctx.createImageData(400, 400)
 
-drawPixel(imageData, 10)
+// drawPixel(imageData, 10, 10)
+for(let x=0; x< 400; ++x)
+{
+    for(let y=0; y< 400; ++y)
+    {
+        drawPixel(imageData, x, y);
+    }
+}
 ctx.putImageData(imageData, 0, 0)
